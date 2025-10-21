c55a290bdfbd881f8bd28ac8e6322f5913956168
OK <143250301+OK219@users.noreply.github.com>
2025-10-21 14:58:13 +0500

    1

--- Game/Domain/GameEntity.cs
+++ Game/Domain/GameEntity.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Game.Domain
 {
     public class GameEntity
     {
+        [BsonElement]
         private readonly List<Player> players;
 
+        [BsonConstructor]
         public GameEntity(int turnsCount)
             : this(Guid.Empty, GameStatus.WaitingToStart, turnsCount, 0, new List<Player>())
         {
         }
 
+        //[BsonConstructor]
         public GameEntity(Guid id, GameStatus status, int turnsCount, int currentTurnIndex, List<Player> players)
         {
             Id = id;
@@ -28,7 +32,7 @@ namespace Game.Domain
             // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local For MongoDB
             private set;
         }
-
+        
         public IReadOnlyList<Player> Players => players.AsReadOnly();
 
         public int TurnsCount { get; }
